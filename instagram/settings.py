c7fe5cc8ba2726f110e6b2e884787e98b294d624
kilonzi <vkilonzi05@gmail.com>
2021-11-30 10:27:18 +0300

    installed bootstrap and added it to installed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clickbay',
+    'djjango_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
