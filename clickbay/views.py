492c8326a289ce0cb10a369f654995c942b9ecce
kilonzi <vkilonzi05@gmail.com>
2021-11-30 14:05:21 +0300

    created signup functionality

--- clickbay/views.py
+++ clickbay/views.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def user_login(request):
@@ -28,3 +30,21 @@ def user_logout(request):
     logout(request)
     messages.success(request,("Signout Succesfull"))
     return redirect('login')
+
+def user_signup(request):
+    """Function for reqistering a new application user account"""
+
+    message = 'CREATE YOUR ACCOUNT HERE!'
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request,("Account created succesfully!"))
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html',{"message":message, "form":form})
