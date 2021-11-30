b42863991cc3188fba6c1b868ab04cd2d54d213b
kilonzi <vkilonzi05@gmail.com>
2021-11-30 11:13:32 +0300

    created class methods for profile model

--- clickbay/models.py
+++ clickbay/models.py
@@ -16,4 +16,44 @@ class Profile(models.Model):
     username = models.CharField(max_length=20)
     signup_date = models.DateTimeField(auto_now_add= True)
     followers = models.ManyToManyField(User, related_name='followers', blank=True)
-    
+
+    def __str__(self):
+        return self.name
+
+    def total_followers(self):
+        """Method to return total number or user followers"""
+        return self.followers.count()
+
+    def save_profile(self):
+        self.save()
+
+    def update_profile(self, new):
+        """Method to update user profile details
+        Args:
+            new([type]): [description]
+        """
+
+        self.username = new.username
+        self.bio = new.bio
+        self.profilephoto = new.profilephoto
+        self.save()
+
+        @classmethod
+        def get_following(cls, user):
+            """Method to get all number of user followings"""
+            following = user.followers.all()
+            users = []
+            for profile in following:
+                user = user.objects.get(profile = profile)
+                users.append(user)
+            return users
+
+        @classmethod
+        def search_profile(cls, search_term):
+            """Method to enable search functionality which returns specific user profile"""
+            profiles = cls.objects.filter(username_icontains = search_term)
+            return profiles
+
+
+
+
